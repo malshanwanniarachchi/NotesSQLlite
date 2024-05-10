45e6b7ea9c77d1d641d93b2613ee2436b251ff47
Malshan Wanniarachchi <146846742+malshanwanniarachchi@users.noreply.github.com>
2024-05-10 22:47:12 +0530

    note activity xml

--- app/src/main/java/com/example/notessqllite/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/notessqllite/NotesDatabaseHelper.kt
@@ -94,4 +94,13 @@ class NotesDatabaseHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_
 
     }
 
+    fun deleteNote(noteId: Int){
+        val db = writableDatabase
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(noteId.toString())
+        db.delete(TABLE_NAME,whereClause,whereArgs)
+        db.close()
+
+    }
+
 }
