f8d4b46899ac172b7594aec7091dc71ce5da0046
Malshan Wanniarachchi <146846742+malshanwanniarachchi@users.noreply.github.com>
2024-05-10 22:33:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notessqllite/Note.kt
@@ -0,0 +1,6 @@
+package com.example.notessqllite
+
+import android.app.assist.AssistContent
+import android.icu.text.CaseMap.Title
+
+data class Note(val id:Int,val title:String,val content:String)
---
+++ app/src/main/java/com/example/notessqllite/NotesAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.notessqllite
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter(private var notes: List<Note>,context: Context) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+    class NoteViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+        val titleTextView : TextView = itemView.findViewById(R.id.titleTextView)
+        val contentTextView : TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton : ImageView = itemView.findViewById(R.id.updateButton)
+
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item,parent,false)
+        return NoteViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.titleTextView.text = note.title
+        holder.contentTextView.text = note.content
+
+        holder.updateButton.setOnClickListener{
+            val intent = Intent(holder.itemView.context,UpdateNoteActivity::class.java).apply {
+                putExtra("note_id",note.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
+
+    }
+
+    fun refreshData(newNote: List<Note>){
+        notes = newNote
+        notifyDataSetChanged()
+    }
+
+}
